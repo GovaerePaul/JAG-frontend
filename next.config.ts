47a161e597a2471d3cde60349e88f54e55f48483
Belgoune <govaere.paul@gmail.com>
2025-09-24 23:21:54 +0200

    chore: update dependencies

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,18 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   distDir: 'out',
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
+  experimental: {
+    esmExternals: 'loose',
+  },
 };
 
 export default nextConfig;
