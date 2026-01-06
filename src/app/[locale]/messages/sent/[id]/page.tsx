ca0f57af68cbce62c38abec05daae88907e9cd5f
Belgoune <govaere.paul@gmail.com>
2026-01-06 19:11:21 +0100

    fix: dynamic routes build

--- src/app/[locale]/messages/sent/[id]/page.tsx
+++ src/app/[locale]/messages/sent/[id]/page.tsx
@@ -1,5 +1,13 @@
 import MessageDetailPage from '@/components/messages/MessageDetailPage';
 
+export function generateStaticParams() {
+  // Return empty array - messages are dynamic and will be handled client-side
+  // This satisfies Next.js requirement for output: export
+  // Note: With output: export, dynamic routes won't be pre-generated
+  // but the build will succeed and pages will be handled client-side
+  return [];
+}
+
 export default function LocaleSentMessageDetailPage() {
   return <MessageDetailPage />;
 }
