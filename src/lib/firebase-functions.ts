a7199a23f49b77570b3d3b27f0d1353262fdaa4d
Belgoune <govaere.paul@gmail.com>
2026-01-06 19:45:48 +0100

    feat: update region

--- src/lib/firebase-functions.ts
+++ src/lib/firebase-functions.ts
@@ -3,7 +3,9 @@
 import { getFunctions, connectFunctionsEmulator, Functions } from 'firebase/functions';
 import app from './firebase';
 
-const functions: Functions = getFunctions(app);
+// Use europe-west1 region for all functions
+const FUNCTIONS_REGION = 'europe-west1';
+const functions: Functions = getFunctions(app, FUNCTIONS_REGION);
 
 if (process.env.NODE_ENV === 'development' && typeof window !== 'undefined') {
   connectFunctionsEmulator(functions, '127.0.0.1', 5001);
