f98aed9770746206584b88d32f9c68c1400ff0e2
Belgoune <govaere.paul@gmail.com>
2026-01-12 22:55:06 +0100

    fix: build

--- src/components/dialogs/ConfirmDialog.tsx
+++ src/components/dialogs/ConfirmDialog.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button, CircularProgress } from '@mui/material';
+import { Button, CircularProgress, Typography } from '@mui/material';
 import { useTranslations } from 'next-intl';
 import BaseDialog from './BaseDialog';
 
